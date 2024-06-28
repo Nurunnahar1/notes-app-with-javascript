dae05b1e1c1f378b0ee813881abc435577769505
Nurunnahar <nurunnahar9081@gmail.com>
2024-06-28 23:41:36 +0600

    note app done

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-const notesContainer = document.querySelectorAll('.notes-container');
-const createBtn = document.querySelectorAll(".btn");
-let notes = document.querySelectorAll(".input-box");
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
 
 function showNotes() {
-    notesContainerinnerHTML = localStorage.getItem("notes");
+  notesContainer.innerHTML = localStorage.getItem("notes") || "";
+  addDeleteEventListeners();
 }
 
 function updateStorage() {
-    localStorage.setItem("notes", notesContainer.innerHTML);
+  localStorage.setItem("notes", notesContainer.innerHTML);
 }
 
 createBtn.addEventListener("click", () => {
-    let inputBox = document.createElement("p");
-    let img = document.createElement("img");
-    inputBox.className = "input-box";
-    inputBox.setAttribute("contenteditable", "true");
-    img.src = "img/delete.png";
-    notesContainer.appendChild(inputBox).appendChild(img);
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "img/delete.png";
+  inputBox.appendChild(img);
+  notesContainer.appendChild(inputBox);
+  updateStorage();
+  addDeleteEventListeners();
 });
- 
-notesContainer.addEventListener("click",function(e){
-    if (e.target.tagName === "IMG") {
-        e.target.parentElement.remove();
-        updateStorage();
-    }
-    else if (e.target.tagName === "p") {
-        notes = document.querySelectorAll(".input-box");
-        notes.forEach(nt => {
-            nt.onkeyup = function () {
-                updateStorage();
-            }
-        })
-     }
+
+function addDeleteEventListeners() {
+  const deleteButtons = document.querySelectorAll(".input-box img");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      button.parentElement.remove();
+      updateStorage();
+    });
+  });
+
+  const notes = document.querySelectorAll(".input-box");
+  notes.forEach((note) => {
+    note.addEventListener("keyup", updateStorage);
+  });
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
 });
- 
-document.addEventListener("keydown", event => {
-    if (event.key === "Enter") {
-        document.execCommand("insertLineBreak");
-        event.preventDefault();
-    }
-})
+
+showNotes();
